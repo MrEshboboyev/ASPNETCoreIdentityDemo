a85c1f362f135cddc2f2c57a6932222edb0e8442
mreshboboyev <mreshboboyev@gmail.com>
2024-06-17 19:09:29 +0500

    DI for identity classes in AccountController

---
+++ ASPNETCoreIdentityDemo/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreIdentityDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        //DI identity classes
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+    }
+}
