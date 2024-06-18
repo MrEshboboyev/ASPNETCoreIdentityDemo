791414f2053c13a7474091359b0d1811983d84fa
mreshboboyev <mreshboboyev@gmail.com>
2024-06-18 19:04:24 +0500

    Created AdministrationController.cs

---
+++ ASPNETCoreIdentityDemo/Controllers/AdministrationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ASPNETCoreIdentityDemo.Controllers
+{
+    public class AdministrationController
+    {
+        // DI RoleManager
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+    }
+}
