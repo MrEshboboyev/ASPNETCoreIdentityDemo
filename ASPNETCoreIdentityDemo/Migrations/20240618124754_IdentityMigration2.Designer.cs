99ab5b4871f52ea4c35b8053f416a8b92c65549e
mreshboboyev <mreshboboyev@gmail.com>
2024-06-18 17:49:35 +0500

    Update Database with ApplicationUser

--- ASPNETCoreIdentityDemo/Migrations/20240618124754_IdentityMigration2.Designer.cs
+++ ASPNETCoreIdentityDemo/Migrations/20240618124754_IdentityMigration2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ASPNETCoreIdentityDemo.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ASPNETCoreIdentityDemo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240618124754_IdentityMigration2")]
+    partial class IdentityMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
