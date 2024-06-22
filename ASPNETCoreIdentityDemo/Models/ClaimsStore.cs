ee5d081641170cb19feaa6b4d3188c46d745e0a1
mreshboboyev <mreshboboyev@gmail.com>
2024-06-22 15:38:39 +0500

    EditUser.cshtml linked for manage claims; Create : ClaimsStore, UserClaim, UserClaimsViewModel; UserCLaim -> ClaimType, IsSelected; UserClaimsViewModel -> UserId, UserClaims fields

---
+++ ASPNETCoreIdentityDemo/Models/ClaimsStore.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace ASPNETCoreIdentityDemo.Models
+{
+    public static class ClaimsStore
+    {
+        // getting all claims
+        public static List<Claim> GetAllClaims()
+        {
+            return new List<Claim>
+            {
+                // other claims added, as you needed
+                new Claim("Create Role", "Create Role"),
+                new Claim("Edit Role", "Edit Role"),
+                new Claim("Delete Role", "Delete Role")
+            };
+        }
+    }
+}
---
+++ ASPNETCoreIdentityDemo/Models/UserClaimsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ASPNETCoreIdentityDemo.Models
+{
+    public class UserClaimsViewModel
+    {
+        public UserClaimsViewModel()
+        {
+            // avoid NullReferenceException, initialize new List for Claims field
+            Claims = new List<UserClaim>(); 
+        }
+
+        public string UserId { get; set; }
+        public List<UserClaim> Claims { get; set; }
+    }
+}
