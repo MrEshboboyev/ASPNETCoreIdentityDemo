6d07192550c28c6a31489779b9d4f4667ec18fde
mreshboboyev <mreshboboyev@gmail.com>
2024-06-23 10:45:44 +0500

    EditRole.cshtml linked for ManageRoleClaims action; Create RoleClaim.cs ->  ClaimType, IsSelected; Create RoleClaimsViewModel -> RoleId, List<RoleClaim> RoleClaims

---
+++ ASPNETCoreIdentityDemo/Models/RoleClaimsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ASPNETCoreIdentityDemo.Models
+{
+    public class RoleClaimsViewModel
+    {
+        public RoleClaimsViewModel()
+        {
+            Claims = new List<RoleClaim>();
+        }
+
+        public string RoleId { get; set; }
+        public List<RoleClaim> Claims { get; set; }
+    }
+}
